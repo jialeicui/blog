62b43ffe9e66f9004cc07ae856c144c0c6fa201a
Jialei <jialeicui@126.com>
2014-11-13 18:35:12 +0800

    hide title & change margin

--- application/config/blog.php
+++ application/config/blog.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['show_title']           = TRUE;
+$config['show_title']           = false;
 
 //第一个%s为title,第二个%s为content, 如果包含转义字符,用双引号包裹
 $config['title_content_format'] = "##%s\n---\n%s";
