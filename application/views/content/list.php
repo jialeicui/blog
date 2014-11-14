cdf75ecfe69b962b227f12806eac9904671e2932
Jialei <jialeicui@126.com>
2014-11-14 10:52:12 +0800

    'remove function' temporarily removed

--- application/views/content/list.php
+++ application/views/content/list.php
@@ -17,11 +17,13 @@
                 </a>
             </div>
             <?php if (isset($loggedin) && $loggedin) { ?>
+                <!--
                 <div class="btn-group">
                     <a class="btn btn-default" type="button">
                         <span class="glyphicon glyphicon-remove text-danger"></span>
                     </a>
                 </div>
+                -->
                 <div class="btn-group">
                     <a class="btn btn-default" type="button" href="<?php echo site_url('status/'.$one->id);?>">
                         <span class="glyphicon text-danger <?php echo get_gly($one->status);?>"></span>
