ca5e9716f3d8c97a36f461b569f027d0ca41bb5b
Jialei <jialeicui@126.com>
2014-11-19 21:38:00 +0800

    add special thanks

--- application/views/foot.php
+++ application/views/foot.php
@@ -1,3 +1,12 @@
+        <footer class="footer_content">
+          Copyright © 2014 - yadam - 
+          Special thanks to
+          <a href="http://ellislab.com/codeigniter" target="_blank">CodeIgniter</a>
+          <a href="http://daringfireball.net/projects/markdown/" target="_blank">Markdown</a>
+          <a href="https://github.com//jonschlinkert/remarkable" target="_blank">Remarkable</a>
+          <a href="https://github.com/isagalaev/highlight.js" target="_blank">Highlight.js</a>
+          <a href="https://github.com/twbs/bootstrap" target="_blank">Bootstrap</a>
+        </footer>
         </div>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/highlight.js/8.2/styles/solarized_light.css">
 	<script src="http://libs.baidu.com/jquery/1.9.1/jquery.min.js"></script>
